5823eff3c4c976ffc920fdec71f6cf4014aa67fa
Cooper Harris <cwharris365@gmail.com>
2025-12-16 14:21:06 -0700

    feat: add centered welcome message to App screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import { Text, View } from 'react-native';
 import './global.css';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
+    </View>
   );
 }
