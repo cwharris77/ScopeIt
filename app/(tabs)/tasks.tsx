7c5aba464be10f59598b8384c9055b240f4d921b
Cooper Harris <cwharris365@gmail.com>
2025-12-20 22:40:33 -0700

    style: add background and flex styling to TasksScreen view

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function TasksScreen() {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-[#0B1220]">
       <Text>Tasks</Text>
     </View>
   );
