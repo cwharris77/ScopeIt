d3fd1320b11dd6aaa0ba7d4158e8ffadde0c0bd8
Cooper Harris <cwharris365@gmail.com>
2025-12-20 18:18:49 -0700

    refactor: update app layout, tabs, and colors with Ionicons and dark theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -5,13 +6,28 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: '#64748B',
-        tabBarInactiveTintColor: '#0B1220',
-        tabBarStyle: { backgroundColor: '#0B1220' }, // Tab bar
-        tabBarItemStyle: { backgroundColor: '#0B1220' }, // Tab bar items
+        tabBarActiveTintColor: '#087f8c',
+        tabBarInactiveTintColor: '#fff',
+        tabBarStyle: { backgroundColor: '#0B1220', borderTopWidth: 0 },
       }}>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="tasks" options={{ title: 'Tasks' }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tasks"
+        options={{
+          title: 'Tasks',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'list-sharp' : 'list-outline'} color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
