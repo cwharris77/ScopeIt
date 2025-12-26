13595338b9b28abe41343aafaacace2330396f45
Cooper Harris <cwharris365@gmail.com>
2025-12-25 21:19:55 -0700

    refactor: replace inline loading view with reusable Loading component in tasks screen

---
+++ components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Loading() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Loading...</Text>
+    </View>
+  );
+}
