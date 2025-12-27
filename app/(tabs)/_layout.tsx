939016398020a1a7cc72319599ab0525462bee00
Cooper Harris <cwharris365@gmail.com>
2025-12-26 21:43:06 -0700

    feat: add custom floating tab bar and update tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+import { FloatingTabBar } from '@/components/FloatingTabBar';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -6,29 +6,12 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: '#087f8c',
-        tabBarInactiveTintColor: '#433e3f',
-        tabBarStyle: { backgroundColor: '#fff', borderTopWidth: 0 },
-        sceneStyle: { backgroundColor: '#fff' },
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="tasks"
-        options={{
-          title: 'Tasks',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'list-sharp' : 'list-outline'} color={color} size={24} />
-          ),
-        }}
-      />
+        sceneStyle: { backgroundColor: '#181922' },
+      }}
+      tabBar={(props) => <FloatingTabBar {...props} />}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="tasks" />
+      <Tabs.Screen name="add" options={{ href: null }} />
     </Tabs>
   );
 }
