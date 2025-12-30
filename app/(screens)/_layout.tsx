07660cd35631a4c16f7682b82d65b12cbd5a74d1
Cooper Harris <cwharris365@gmail.com>
2025-12-30 15:18:25 -0700

    refactor: move add-task file to modals group

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from 'expo-router';
 export default function ScreensLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="add-task" />
       <Stack.Screen name="edit-task" />
     </Stack>
   );
