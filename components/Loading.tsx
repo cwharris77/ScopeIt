5f1642665ddace0fb2982a2b37764c67558da3a1
Cooper Harris <cwharris365@gmail.com>
2026-01-01 18:06:31 -0700

    feat: new user-facing features only; refactor: code improvements; fix: bug fixes;

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,10 @@
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Spinner } from 'tamagui';
 
 export default function Loading() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>Loading...</Text>
+      <Spinner size="large" color="" />
     </SafeAreaView>
   );
 }
